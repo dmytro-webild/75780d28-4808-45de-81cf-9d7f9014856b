b958fb1dcbcbdc40f41d18b40d30457fd21fbb72
bender <kudin.dima@gmail.com>
2026-04-10 16:59:30 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,22 +42,10 @@ export default function LandingPage() {
             background={{ variant: "gradient-bars" }}
             title="Drawing smiles on your face."
             description="We are Hàlo games, a premier Roblox development studio crafting the next generation of fashion and runway experiences for millions of players."
-            testimonials={[
-              { name: "Alex P.", handle: "@showoff_pro", testimonial: "The best runway game on Roblox ever!", rating: 5, imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3C4kmKEUQIDYX1gzW6F8WkxPlcT/uploaded-1775839331625-5uswmgnw.png", imageAlt: "User provided image" },
-              { name: "Jamie L.", handle: "@fashionqueen", testimonial: "Can't wait for Makeup Stars to drop!", rating: 5, imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3C4kmKEUQIDYX1gzW6F8WkxPlcT/uploaded-1775839331625-ktwug3ih.png?_wi=1", imageAlt: "User provided image" },
-              { name: "Sam R.", handle: "@roblox_dev", testimonial: "Hàlo games always delivers perfection.", rating: 5, imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3C4kmKEUQIDYX1gzW6F8WkxPlcT/uploaded-1775839331625-70df7kfk.png?_wi=1", imageAlt: "User provided image" },
-              { name: "Taylor B.", handle: "@runway_king", testimonial: "Amazing graphics and gameplay loop.", rating: 5, imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3C4kmKEUQIDYX1gzW6F8WkxPlcT/an-ethereal-nebula-inspired-background-w-1775839360199-e44a393f.png?_wi=1", imageAlt: "An ethereal nebula-inspired background with subtle purple and blue gradient light rays. A clean, mod" },
-              { name: "Riley M.", handle: "@makeup_fan", testimonial: "Simply the best experience I've had.", rating: 5, imageSrc: "http://img.b2bpic.net/free-photo/cheerful-young-man-playing-video-game-tablet_1262-4766.jpg?_wi=1", imageAlt: "happy gamer portrait" },
-            ]}
+            testimonials={[]}
             imageSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3C4kmKEUQIDYX1gzW6F8WkxPlcT/an-ethereal-nebula-inspired-background-w-1775839360199-e44a393f.png?_wi=2"
             mediaAnimation="blur-reveal"
-            avatars={[
-              { src: "http://img.b2bpic.net/free-photo/cheerful-young-man-playing-video-game-tablet_1262-4766.jpg", alt: "Gamer avatar 1" },
-              { src: "http://img.b2bpic.net/free-photo/gaming-setup-with-laptop-desk-still-life_23-2149829155.jpg", alt: "Gamer avatar 2" },
-              { src: "http://img.b2bpic.net/free-photo/black-american-woman-holding-basketball_23-2148773333.jpg", alt: "Gamer avatar 3" },
-              { src: "http://img.b2bpic.net/free-photo/full-shot-happy-man-winning-game_23-2149005177.jpg", alt: "Gamer avatar 4" },
-              { src: "http://img.b2bpic.net/free-photo/side-view-friends-playing-videogame_23-2149350036.jpg", alt: "Gamer avatar 5" },
-            ]}
+            avatars={[]}
             marqueeItems={[
               { type: "text", text: "Fashion Forward" },
               { type: "text", text: "Roblox Experts" },
